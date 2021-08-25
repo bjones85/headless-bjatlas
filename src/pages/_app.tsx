b43d7c6c10517638d851ded92149873348e5342e
Brendan J <brendan.jones@wpengine.com>
2021-08-25 15:15:38 -0500

    Updated _apps.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,11 @@ import 'scss/main.scss';
 import { client } from 'client';
 import type { AppProps } from 'next/app';
 
+headlessConfig({
+  wpUrl: process.env.WORDPRESS_URL || process.env.NEXT_PUBLIC_WORDPRESS_URL,
+  apiClientSecret: process.env.WP_HEADLESS_SECRET,
+});
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
